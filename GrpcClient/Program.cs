0165a60743515d2ddd19288b2020a55a33d8581e
patel4585 <dpl190@uregina.ca>
2025-11-30 15:44:47 -0600

    Initial Commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "World!" };
+            var channel = GrpcChannel.ForAddress("http://localhost:5070");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            // Just to stop the program from closing.
+            Console.ReadLine(); 
+        }
+    }
+}
